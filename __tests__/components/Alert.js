6038e4f0bc739807fe6e94d446cb4e8cf0b5f31d
Vincent Langlet <vincentl@theodo.fr>
2017-01-07 14:30:01 +0100

    :rotating_light: Add first test

---
+++ __tests__/components/Alert.js
@@ -0,0 +1,28 @@
+import Alert from 'components/Alert'
+import React from 'react'
+
+import { shallow } from 'enzyme'
+import toJSON from 'enzyme-to-json'
+
+describe('Alert', () => {
+  it('should handle 403 error ', () => {
+    const error = { code: 403 };
+    const alert = shallow(<Alert error={ error } />);
+
+    expect(toJSON(alert)).toMatchSnapshot();
+  });
+
+  it('should handle 404 error ', () => {
+    const error = { code: 404 };
+    const alert = shallow(<Alert error={ error } />);
+
+    expect(toJSON(alert)).toMatchSnapshot();
+  });
+
+  it('should handle default error ', () => {
+    const error = { code: 0 };
+    const alert = shallow(<Alert error={ error } />);
+
+    expect(toJSON(alert)).toMatchSnapshot();
+  });
+});
