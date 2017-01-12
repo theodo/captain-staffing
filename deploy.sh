14d8d4fa9647bef471b2283aec730ab0c6658ec3
Vincent Langlet <vincentl@theodo.fr>
2017-01-12 23:54:27 +0100

    :wrench: Do not use linter for deployment

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 git stash save
 git checkout -B gh-pages
 git add -f build
-git commit -am "Rebuild website"
+git commit -n -am "Rebuild website"
 git filter-branch -f --prune-empty --subdirectory-filter build
 git push -f origin gh-pages
 git checkout -
