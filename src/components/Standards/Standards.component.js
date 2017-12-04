50506e67d592359ddd42230fdb2a7f00c7cbcfcf
Benjamin Grandfond <benjaming@theodo.fr>
2017-12-04 01:00:25 +0100

    Make Standards a standard component

---
+++ src/components/Standards/Standards.component.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Standards extends Component {
+  render() {
+    const standards = this.props.weeks.map((week) => {
+      const classNames = ['planning-cell', 'weekly-standard'];
+
+      if (Object.prototype.hasOwnProperty.call(this.props.weeklyTasksCount, week.format('w')) && this.props.weeklyTasksCount[week.format('w')] > this.props.user.standards.projects) {
+        classNames.push('error');
+      }
+
+      return (
+        <div key={week.format('w')} className={classNames.join(' ')} />
+      );
+    });
+
+    return (
+      <div className="standards">{standards}</div>
+    );
+  }
+}
+
+Standards.propTypes = {
+  user: PropTypes.shape({
+    standards: PropTypes.shape({
+      projects: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+  tasks: PropTypes.array.isRequired,
+  weeks: PropTypes.array.isRequired,
+  weeklyTasksCount: PropTypes.object.isRequired,
+};
---
+++ src/components/Standards/Standards.container.js
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import Standards from './Standards.component';
+
+const mapStateToProps = () => ({});
+
+const mapDispatchToProps = () => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Standards);
+
